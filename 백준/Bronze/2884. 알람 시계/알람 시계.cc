cead41af780cdb9c8b589f8085f15a71512eecc1
김찬빈 (Kim ChanBin) <106393694+Noonsom@users.noreply.github.com>
2022-11-20 14:01:29 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    
+    std::cin >> a >> b;
+    
+    if (b < 45) 
+    {
+        b += 15;
+        if (--a < 0) a = 23;
+    }
+    else b -= 45;
+
+    std::cout << a << " " << b;
+}
