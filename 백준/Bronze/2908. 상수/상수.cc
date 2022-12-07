ca60dfd8cf4e1b8e836a2242a111cc5c5bd4a0de
김찬빈 (Kim ChanBin) <106393694+Noonsom@users.noreply.github.com>
2022-12-07 09:03:02 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string A, B;
+
+	cin >> A >> B;
+	reverse(A.begin(), A.end());
+	reverse(B.begin(), B.end());
+	
+	if (stoi(A) > stoi(B)) cout << A;
+	else cout << B;
+}
