6290aea0fa3e3ccfd1d8e44b9a88ab873cd5ab31
ChanBin Kim <cong8685@naver.com>
2023-06-25 16:20:33 +0900

    [unrated] Title: 문자열 출력하기, Time: 2.00 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str; // 입력 데이터를 저장 할 변수 선언
+    cin >> str; // cin을 통해 데이터 입력
+    cout << str; // cout를 통해 데이터 출력
+    return 0;
+}
