58544a1d60d0ed4afcb4e87d34b7723e4e1940dc
ChanBin Kim <cong8685@naver.com>
2024-01-26 13:20:05 +0900

    [Silver IV] Title: 큐, Time: 348 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	queue<int> q;
+	int count;
+
+	cin >> count;
+	cin.ignore();
+
+	while(count != 0)
+	{
+		string input;
+		getline(cin, input);
+
+		if (input == "pop")
+		{
+			if (q.size() != 0)
+			{
+				cout << q.front() << "\n";
+				q.pop();
+			}
+			else cout << "-1\n";
+		}
+		else if (input == "size") cout << q.size() << "\n";
+		else if (input == "empty")
+		{
+			if (q.size() != 0) cout << "0\n";
+			else cout << "1\n";
+		}
+		else if (input == "front")
+		{
+			if (q.size() != 0) cout << q.front() << "\n";
+			else cout << "-1\n";
+		}
+		else if (input == "back")
+		{
+			if (q.size() != 0) cout << q.back() << "\n";
+			else cout << "-1\n";
+		}
+		else q.push(stoi(input.substr(5)));
+
+		count--;
+	}
+}
