9fb47f4dd15e28fbf7d22e21cbde2422ca65d9ac
ChanBin Kim <cong8685@naver.com>
2024-01-31 15:53:00 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int A, B;
+
+	while (!(cin >> A >> B).eof()) cout << A + B << "\n";
+
+	return 0;
+}
