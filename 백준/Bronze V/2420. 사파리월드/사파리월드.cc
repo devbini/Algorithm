ef3139de12659765e36ef1369739d0d0324c53a3
ChanBin Kim <cong8685@naver.com>
2024-02-15 12:59:04 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+
+	long long int A, B;
+	cin >> A >> B;
+
+	cout << abs(A - B);
+
+	return 0;
+}
