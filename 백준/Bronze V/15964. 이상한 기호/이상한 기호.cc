943506a8b101f6edbf8dc18652444f832db68924
ChanBin Kim <cong8685@naver.com>
2024-02-15 12:54:56 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze V/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+
+	int A, B;
+	cin >> A >> B;
+
+	cout << (A + B) * (A - B);
+
+	return 0;
+}
